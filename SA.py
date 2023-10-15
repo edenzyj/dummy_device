42aea681eaeae18318774d46521c1e1dbfcb7c40
edenzyj <edenzyj88@gmail.com>
2023-10-15 17:05:31 +0800

    practice by myself

--- SA.py
+++ SA.py
@@ -1,24 +1,26 @@
 import random 
+import datetime
 
-ServerURL = 'IoTtalk server url' #For example: 'https://iottalk.tw'
-MQTT_broker = None # MQTT Broker address, for example:  'iottalk.tw' or None = no MQTT support
-MQTT_port = 1883
-MQTT_encryption = False
-MQTT_User = 'User'
-MQTT_PW = 'PW'
+ServerURL = 'https://3.iottalk.tw' #For example: 'https://iottalk.tw'
+MQTT_broker = '3.iottalk.tw' # MQTT Broker address, for example:  'iottalk.tw' or None = no MQTT support
+MQTT_port = 5566
+MQTT_encryption = True
+MQTT_User = 'iottalk'
+MQTT_PW = 'iottalk2023'
 
-device_model = 'Dummy_Device'
-IDF_list = ['Dummy_Sensor']
-ODF_list = ['Dummy_Control']
-device_id = None #if None, device_id = MAC address
-device_name = None
+device_model = '312833017_new'
+IDF_list = ['I312833017']
+ODF_list = ['O312833017']
+device_id = '31283301710151649' #if None, device_id = MAC address
+device_name = 'eden_device'
 exec_interval = 1  # IDF/ODF interval
 
 def on_register(r):
     print('Server: {}\nDevice name: {}\nRegister successfully.'.format(r['server'], r['d_name']))
 
-def Dummy_Sensor():
-    return random.randint(0, 100), random.randint(0, 100), random.randint(0, 100), random.randint(0, 100) 
+def I312833017():
+    return '312833017' + str(datetime.datetime.now())
+    #return random.randint(0, 100)
 
-def Dummy_Control(data:list):
+def O312833017(data:list):
     print(data[0])
